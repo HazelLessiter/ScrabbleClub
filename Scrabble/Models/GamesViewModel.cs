e95ac4d6d7dc37cef0adcbd1ea91b7963606f88c
Hazel Lessiter <39559927+HazelLessiter@users.noreply.github.com>
2018-05-30 17:55:28 +0100

    Add files via upload

---
+++ Scrabble/Models/GamesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrabble.Models
+{
+    public class GamesViewModel
+    {
+        public string MemberName { get; set; }
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+    }
+}
